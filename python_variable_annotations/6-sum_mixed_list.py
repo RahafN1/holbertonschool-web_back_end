241d817328ee0bfca9de9dc69b081f91ad9196ad
RahafN1 <rahafalabdalh24@gmail.com>
2026-08-26 14:27:38 +0300

    add file 6-sum_mixed_list.py

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that sums a list of floats and integers."""
+
+
+def sum_mixed_list(mxd_lst: list) -> float:
+    """Return the sum of all floats and integers in the given list."""
+    return sum(mxd_lst)
