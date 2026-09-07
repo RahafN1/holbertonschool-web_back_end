f34137e47d39a1525b8615412524ab020c15ee69
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-07 16:59:09 +0300

    add file

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-
+"""Simple pagination
+"""
 import csv
 import math
-from typing import List
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return a tuple of start and end index for the given pagination
+    parameters.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
 
 
 class Server:
@@ -25,13 +35,17 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return the page of the dataset corresponding to the given
+        page and page_size arguments.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
+        start_index, end_index = index_range(page, page_size)
         dataset = self.dataset()
-        start, end = index_range(page, page_size)
 
-        if start >= len(dataset):
+        if start_index >= len(dataset):
             return []
 
-        return dataset[start:end]
+        return dataset[start_index:end_index]
+    
