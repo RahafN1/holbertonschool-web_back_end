94859d79cdf351e903af611596168affdb520b73
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-10 10:00:28 +0300

    addd file 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-"""Script that provides stats about Nginx logs stored in MongoDB"""
+#!/usr/bin/python3
+"""Python script that provides some stats about Nginx logs stored in MongoDB"""
 from pymongo import MongoClient
 
 
@@ -7,23 +7,17 @@ if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx_collection = client.logs.nginx
 
-    print("{} logs".format(nginx_collection.count_documents({})))
+    total_logs = nginx_collection.count_documents({})
+    print(f"{total_logs} logs")
+
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: {count}")
 
     status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"})
-    print("{} status check".format(status_check))
-
-    print("IPs:")
-    ips = nginx_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ])
-
-    for ip in ips:
-        print("\t{}: {}".format(ip["_id"], ip["count"]))
+        {"method": "GET", "path": "/status"}
+    )
+    print(f"{status_check} status check")
+    
