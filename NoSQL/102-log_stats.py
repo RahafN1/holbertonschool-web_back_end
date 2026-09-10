ff567d0ba7672a4a84b76281e55c408c5adaa401
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-10 11:25:40 +0300

    add file

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -1,32 +1,46 @@
 #!/usr/bin/env python3
-"""102-log_stats.py - Improved log stats with top 10 IPs"""
+"""Script that provides stats about Nginx logs stored in MongoDB."""
+
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    nginx_collection = client.logs.nginx
+    collection = client.logs.nginx
 
-    n_logs = nginx_collection.count_documents({})
-    print(f"{n_logs} logs")
-
+    print("{} logs".format(collection.count_documents({})))
     print("Methods:")
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = nginx_collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
 
-    status_checks = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_checks} status check")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    for method in methods:
+        count = collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, count))
+
+    status = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print("{} status check".format(status))
 
     print("IPs:")
+
     pipeline = [
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}},
-        {"$limit": 10}
+        {
+            "$group": {
+                "_id": "$ip",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {
+                "count": -1
+            }
+        },
+        {
+            "$limit": 10
+        }
     ]
-    top_ips = nginx_collection.aggregate(pipeline)
-    for ip in top_ips:
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+
+    for ip in collection.aggregate(pipeline):
+        print("\t{}: {}".format(ip["_id"], ip["count"]))
