2916f5f9d437ce2b1c4d49182de615058e731de0
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-22 15:21:07 +0300

    add file 100-await.js

--- ES6_promise/100-await.js
+++ ES6_promise/100-await.js
@@ -1,20 +1,13 @@
-```js
 import { uploadPhoto, createUser } from './utils.js';
 
-export default async function asyncUploadUser() {
+async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-
-    return {
-      photo,
-      user,
-    };
+    return { photo, user };
   } catch (error) {
-    return {
-      photo: null,
-      user: null,
-    };
+    return { photo: null, user: null };
   }
 }
-```
+
+export default asyncUploadUser;
