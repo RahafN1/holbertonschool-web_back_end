0f7d64ac5c3b4416ed5c0a9dc0bbcfa4e0c88ebf
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-22 12:43:12 +0300

    add file 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,8 +2,13 @@ import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
+  const signUpPromise = signUpUser(firstName, lastName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((err) => ({ status: 'rejected', value: err.toString() }));
+
+  const uploadPhotoPromise = uploadPhoto(fileName)
+    .then((value) => ({ status: 'fulfilled', value }))
+    .catch((err) => ({ status: 'rejected', value: err.toString() }));
+
+  return Promise.all([signUpPromise, uploadPhotoPromise]);
 }
