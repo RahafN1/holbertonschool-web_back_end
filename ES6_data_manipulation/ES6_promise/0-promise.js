dc7c9d458087e8f604dfaddae316fe9b33c2e4a8
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-22 12:03:03 +0300

    add file 0-promise.js

--- ES6_data_manipulation/ES6_promise/0-promise.js
+++ ES6_data_manipulation/ES6_promise/0-promise.js
@@ -1,5 +1,3 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve();
-  });
+export default async function getResponseFromAPI(){
+    return 'leen';
 }
