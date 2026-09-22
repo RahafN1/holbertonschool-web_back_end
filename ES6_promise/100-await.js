cc10a254f25adf05a11736d50eea544c6f339c2e
RahafN1 <rahafalabdalh24@gmail.com>
2026-09-22 14:35:34 +0300

    add file 100-await.js

---
+++ ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+```js
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
+}
+```
